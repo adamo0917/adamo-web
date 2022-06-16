88ad2772dd31ca3ebe77d8115eedb2a7662b8413
adamo <adam.ondrejcak@gmail.com>
2022-06-16 17:13:01 +0200

    base page

---
+++ index.php
@@ -0,0 +1,13 @@
+ <?php
+    $name = "adamo's web";
+    $ver = "prototype 1";
+ ?>
+ <!DOCTYPE HTML>
+ <html>
+    <head>
+        <title>Home - <?php echo $name;?></title>
+    </head>
+    <body>
+        <?php echo $ver;?>
+    </body>
+ </html>
